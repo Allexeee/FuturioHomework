c787467a9c1065a0a3b69cc7b7e92235a50d1cdf
Allexeee <aleksey_ohezin@mail.ru>
2022-02-28 23:43:08 +0200

    refactoring

--- FuturioHomework/IQueueCommands.cs
+++ FuturioHomework/IQueueCommands.cs
@@ -1,57 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace FuturioHomework
+﻿namespace FuturioHomework
 {
   public interface IQueueCommands
   {
     void Add(ICommand    command);
-    void Remove(ICommand command);
-  }
-
-  public class QueueCommands : IQueueCommands
-  {
-    Queue<ICommand> _commands = new Queue<ICommand>();
-
-    public void Add(ICommand command)
-    {
-      _commands.Enqueue(command);
-    }
-
-    public void Remove(ICommand command)
-    {
-      var commands = new ICommand[_commands.Count];
-      var i        = 0;
-      while (_commands.TryDequeue(out var c))
-      {
-        if(c == command) continue;
-        commands[i++] = c;
-      }
-
-      for (var j = 0; j < i; j++)
-      {
-        _commands.Enqueue(commands[j]);
-      }
-    }
-  }
-  
-  public class QueueCommandsExample : ICommand, IQueueCommands
-  {
-    List<ICommand> _commands = new List<ICommand>();
-
-    public void Add(ICommand command)
-    {
-      _commands.Add(command);
-    }
-
-    public void Remove(ICommand command)
-    {
-      _commands.Remove(command);
-    }
-
-    public void Execute()
-    {
-      foreach (var command in _commands) 
-        command.Execute();
-    }
   }
 }
---
+++ FuturioHomework/SpaceShip.cs
@@ -0,0 +1,54 @@
+﻿namespace FuturioHomework
+{
+  public class SpaceShip : ICommand
+  {
+    UObject        _obj;
+    IQueueCommands _queue;
+
+    int                _state;
+    ICommandInjectable _commandRotate;
+    ICommandInjectable _commandMove;
+
+    public SpaceShip(UObject obj, IQueueCommands queue)
+    {
+      _obj   = obj;
+      _queue = queue;
+      var commandMoveStart   = new CommandMoveStart(new AdapterMoveStart(_obj, _queue, 1));
+      var commandRotateStart = new CommandRotateStart(new AdapterRotateStart(_obj, queue, 0, 1));
+      _commandMove   = commandMoveStart.CommandMove;
+      _commandRotate = commandRotateStart.CommandRotate;
+      new CommandStart(new AdapterStart(commandMoveStart, commandRotateStart)).Execute();
+    }
+
+    public void Execute()
+    {
+      if (GameTime.Current < 5f)
+        return;
+      TrySetState(1);
+      if (GameTime.Current < 10f)
+        return;
+      TrySetState(2);
+    }
+
+    void TrySetState(int state)
+    {
+      if (state <= _state) 
+        return;
+
+      _state = state;
+
+      switch (_state)
+      {
+        case 1:
+          var commandMoveEnd   = new CommandMoveEnd(new AdapterMoveEnd(_obj, _commandMove));
+          var commandRotateEnd = new CommandRotateEnd(new AdapterRotateEnd(_obj, _commandRotate));
+          new CommandEnd(new AdapterEnd(commandMoveEnd, commandRotateEnd)).Execute();
+          break;
+        case 2:
+          new CommandSetAngle(new AdapterSetAngle(_obj, 0)).Execute();
+          new CommandMoveStart(new AdapterMoveStart(_obj, _queue, 1)).Execute();
+          break;
+      }
+    }
+  }
+}
